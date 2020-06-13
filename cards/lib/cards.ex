7d25d752493be0d5df9cce3415c4715d298b6afd
Suki Song <seul.song@avocadomattres.com>
2020-06-13 09:03:04 -0700

    1.12

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,5 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "hi there!"
   end
 end
