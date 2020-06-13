ceb54c602de282964cdda0fa194189cbd4b5d0bc
Suki Song <seul.song@avocadomattres.com>
2020-06-13 10:02:54 -0700

    1.12

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,11 @@
 defmodule Cards do
-
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      suit
+    end
   end
 
 def shuffle(deck) do
@@ -11,5 +15,6 @@ end
 def contains?(deck, card) do
   Enum.member?(deck, card)
 end
+
 
 end
