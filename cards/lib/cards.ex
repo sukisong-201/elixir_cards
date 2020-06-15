7fb68b5adcc2f647415672920453b112588fe90a
Suki Song <seul.song@avocadomattres.com>
2020-06-15 07:38:46 -0700

    end of cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,6 +3,10 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
