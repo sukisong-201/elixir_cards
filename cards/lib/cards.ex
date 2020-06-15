e836ae86a3780afe0dd827d55b51e07a7029851e
Suki Song <seul.song@avocadomattres.com>
2020-06-14 20:57:58 -0700

    2.20

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -26,12 +26,9 @@ def save(deck, filename) do
 end
 
 def load(filename) do
-  {status, binary} = File.read(filename)
-
-  case status do
-    :ok -> :erlang.binary_to_term binary
-    :error -> "That file does not exist"
+  case File.read(filename) do
+    {:ok, binary} -> :erlang.binary_to_term binary
+    {:error, _reason} -> "That file does not exist"
   end
 end
-
 end
