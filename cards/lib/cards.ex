c6533949de9feca57706d20b4cef1d265c9a53f3
sukisong-201 <seul.song@avocadomattres.com>
2020-06-18 10:12:44 -0700

    final code with docs and testing

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -20,6 +20,17 @@ def shuffle(deck) do
   Enum.shuffle(deck)
 end
 
+@doc """
+  Determines whether a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+"""
+
 def contains?(deck, card) do
   Enum.member?(deck, card)
 end
